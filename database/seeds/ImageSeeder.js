7981b9780aebea5092c1de971c008f0625097abe
Wilson Goode <wilsongoode97@gmail.com>
2019-08-15 15:41:18 -0700

    switched to bing and fixed

--- database/seeds/ImageSeeder.js
+++ database/seeds/ImageSeeder.js
@@ -15,7 +15,6 @@ const Factory = use("Factory");
 const Database = use("Database");
 const Scraper = require("images-scraper"),
  bing = new Scraper.Bing();
-  //google = new Scraper.Google();
 
 class ImageSeeder {
   async run() {
@@ -24,46 +23,27 @@ class ImageSeeder {
     // copy image urls into the images table of database
     const numImagesToScrape = 20;
     try {
-      console.log("line 26, trying to insert dummy...");
-      const dummy = await Database.table("images").insert({
-        height: 984,
-        width: 980,
-        type: "image/png",
-        img_url:
-          "https://66.media.tumblr.com/d881dc2ca9b9cd0e47e2d63e91feae3d/tumblr_p4wxbpYPPE1usi9s5o1_1280.png",
-        created_at: Database.fn.now(),
-        updated_at: Database.fn.now()
-      });
-      console.log("dummy: ", dummy);
-      console.log("line 37, trying to scrape (with bing)...");
-      // const scrape = await google.list({
-      //   keyword: "sad animal facts site:sadanimalfacts.com",
-      //   num: numImagesToScrape,
-      //   detail: true,
-      //   nightmare: {
-      //     show: false //hides the chromium window that nightmare uses
-      //   }
-      // });
-      
+      console.log("line 27, trying to scrape (with bing)...");
 
-      const scrape = bing.list({
+      const scrape = await bing.list({
         keyword: 'sad animal facts site:sadanimalfacts.com',
         num: numImagesToScrape,
         detail: true
       })
-      console.log("line 46, got a list ...");
-      console.log(`first ${numImagesToScrape} results from google`);
+      console.log("line 34, got a list ...");
+      console.log(`first ${scrape.length} results from bing`);
+      // console.log(scrape);
+      
       for (let i = 0; i < scrape.length; i++) {
-        console.log("line 49, url for image", i);
-        console.log(image.url);
-
+        // console.log("line 39, url for image", i);
+        
         const image = scrape[i];
         // console.log(image.url);
         // console.log(i);
         const imageId = await Database.table("images").insert({
           height: image.height,
           width: image.width,
-          type: image.type,
+          type: image.format,
           img_url: image.url,
           created_at: Database.fn.now(),
           updated_at: Database.fn.now()
